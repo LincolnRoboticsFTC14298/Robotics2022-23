a78e37f5f68b37d427df9ee7098e7758bb913b5c
FyreWolf99 <78391828+FyreWolf99@users.noreply.github.com>
2022-09-25 20:53:59 -0700

    Add Audio Analyzer
    might finish eventually

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AudioAnalyzer.kt
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode
+
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+
+import kotlin.math.abs;
+
+class AudioAnalyzer
+{
+    private var ar: AudioRecord? = null;
+    private var minSize = 0;
+
+    fun start()
+    {
+        minSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        ar = AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
+        ar!!.startRecording();
+    }
+
+    fun stop()
+    {
+        ar!!.stop();
+    }
+
+    fun getAmplitude(): Int
+    {
+        val buffer = ShortArray(minSize);
+        ar!!.read(buffer, 0, minSize);
+        var max = 0;
+        for (s in buffer)
+        {
+            if (abs(s.toInt()) > max)
+            {
+                max = abs(s.toInt());
+            }
+        }
+        return max;
+    }
+}
