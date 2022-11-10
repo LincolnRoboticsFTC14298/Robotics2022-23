1348632e429bf09db47f138cb05014f316b0fe0d
tony <tony.riggioni@gmail.com>
2022-11-09 16:31:41 -0800

    Implemented Passthrough

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Passthrough.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Passthrough.kt
@@ -9,29 +9,34 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 /**
  * Passthrough subsystem consist of a servo that rotates
  * the mechanism containing the intake/claw from pick up to drop off.
- * @param hardwareMap HardwareMap
+ * @param hwMap HardwareMap
  * @param servoName Passthrough's servo's name
  */
-class Passthrough(hardwareMap: HardwareMap, servoName: String) : SubsystemBase() {
+class Passthrough(hwMap: HardwareMap, servoName: String) : SubsystemBase()
+{
 
     /**
      * TODO: Set min and max degrees.
      * @see <a href="https://docs.ftclib.org/ftclib/features/hardware">FTCLib Docs: Hardware</a>
      */
-    private val servo: ServoEx = SimpleServo(hardwareMap, servoName, 0.0, 1.0)
+    private val servo: ServoEx = SimpleServo(hwMap, servoName, 0.0, 1.0)
+
+    private val MIN = 0.0;
+    private val MAX = 1.0;
 
     /**
      * Rotate servo to drop off position.
      */
-    fun drop() {
-
+    fun drop()
+    {
+        servo.position = MAX;
     }
 
     /**
      * Rotate servo to pick up cone.
      */
-    fun pickUp() {
-
+    fun pickUp()
+    {
+        servo.position = MIN;
     }
-
 }
