c7f31fdd68fe56dc2d4e4b5e88fa93b16950e821
Jared Haertel <jared.haertel@gmail.com>
2022-11-17 23:01:43 -0800

    Implemented getHeight method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.kt
@@ -11,32 +11,34 @@ import com.qualcomm.robotcore.hardware.HardwareMap
  * @param leftMotorName Lift's left motor's name.
  * @param rightMotorName Lift's right motor's name.
  */
-class Lift(hardwareMap: HardwareMap, leftMotorName: String, rightMotorName: String)  : SubsystemBase()
-{
+class Lift(hardwareMap: HardwareMap, leftMotorName: String, rightMotorName: String)  : SubsystemBase() {
 
     /**
      * Avoid using the individual motors, it's best to use the group.
-     * TODO: reverse motor if appropriate
+     * TODO: reverse motor if appropriate.
      * @see <a href="https://docs.ftclib.org/ftclib/features/hardware/motors">FTCLib Docs: Motors</a>
      */
     private val leftMotor  = Motor(hardwareMap, leftMotorName)
     private val rightMotor = Motor(hardwareMap, rightMotorName)
-    private val motorGroup = MotorGroup(leftMotor, rightMotor) // TODO: Set zero power behavior to break
+    private val motorGroup = MotorGroup(leftMotor, rightMotor)
+
+    init {
+        motorGroup.setDistancePerPulse(1.0) // TODO: Experimentally find value.
+        motorGroup.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE)
+    }
 
     /**
      * Set power of lift.
      * @param power Percentage of the maximum speed of the lift.
      */
-    fun setPower(power: Double)
-    {
+    fun setPower(power: Double) {
         motorGroup.set(power)
     }
 
     /**
      * Stop lift.
      */
-    fun stop()
-    {
+    fun stop() {
         motorGroup.stopMotor()
     }
 
@@ -44,8 +46,8 @@ class Lift(hardwareMap: HardwareMap, leftMotorName: String, rightMotorName: Stri
      * Get vertical component of the lift extension using encoder ticks.
      * @return Height of the lift in cm.
      */
-    fun getHeight(): Double
-    {
-        return 0.0
+    fun getHeight(): Double {
+        return motorGroup.distance
     }
+
 }
