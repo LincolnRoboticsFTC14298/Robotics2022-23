b90e648a1704c861bdd49b66f443732576a29f43
Jared Haertel <jared.haertel@gmail.com>
2022-11-25 12:04:03 -0800

    Moved names to RobotConfig.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.kt
@@ -4,7 +4,9 @@ import com.arcrobotics.ftclib.command.SubsystemBase
 import com.arcrobotics.ftclib.hardware.motors.CRServo
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.TouchSensor
+import org.firstinspires.ftc.teamcode.RobotConfig.intakeServoName
 import org.firstinspires.ftc.teamcode.RobotConfig.intakeSpeed
+import org.firstinspires.ftc.teamcode.RobotConfig.intakeTouchSensorName
 
 /**
  * Intake subsystem consists of a continuous rotating servo
@@ -13,13 +15,13 @@ import org.firstinspires.ftc.teamcode.RobotConfig.intakeSpeed
  * @param servoName Intake continuous servo's name.
  * @param sensorName Touch Sensor Name
  */
-class Intake(hwMap: HardwareMap, servoName: String, sensorName: String) : SubsystemBase() {
+class Intake(hwMap: HardwareMap) : SubsystemBase() {
 
     /**
      * @see <a href="https://docs.ftclib.org/ftclib/features/hardware/motors">FTCLib Docs: Motors</a>
      */
-    private val servo: CRServo = CRServo(hwMap, servoName)
-    private val sensor: TouchSensor = hwMap.get(TouchSensor::class.java, sensorName)
+    private val servo: CRServo = CRServo(hwMap, intakeServoName)
+    private val sensor: TouchSensor = hwMap.get(TouchSensor::class.java, intakeTouchSensorName)
 
     /**
      * Turn servo on at a constant speed to pick up cone.
