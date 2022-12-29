daf48c4cacb3b915c45c2243fdb62c51ff6b7a90
Jared Haertel <jared.haertel@gmail.com>
2022-12-28 23:48:34 -0800

    I did stuff idr.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.kt
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems
 
+import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.arcrobotics.ftclib.command.SubsystemBase
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.teamcode.vision.ConePipeline
 import org.firstinspires.ftc.teamcode.vision.PolePipeline
+import org.opencv.core.Point
 import org.openftc.easyopencv.OpenCvCameraFactory
 import org.openftc.easyopencv.OpenCvInternalCamera
 import org.openftc.easyopencv.OpenCvPipeline
@@ -79,17 +81,21 @@ class Vision(
      * @return Coordinate of the closest cone in the robot tangent space.
      */
     fun getConeRelativePosition(): Vector2d? {
-        // TODO
-        return Vector2d(0.0, 0.0)
+        TODO("Implement")
     }
 
     /**
-     * @return List of relative position of the poles in the robot tangent space,
-     *         sometimes using cones instead of the poles themselves as reference points.
+     * @return List of pixel info for landmarks form pipeline
      */
-    fun getPoleRelativePositions(): List<Vector2d> {
-        // TODO
-        return emptyList()
+    fun getLandmarkInfo(): List<Point> {
+        TODO("Implement")
+    }
+
+    /**
+     * @return Returns global position from pixel space
+     */
+    fun pixelToGlobalPosition(pixel: Point, realHeight: Double, position: Pose2d) : Vector2d {
+        TODO("Implement")
     }
 
 }
