e9c98767ad07ac080b83532f7e3d831606364613
Jared Haertel <jared.haertel@gmail.com>
2023-01-21 22:40:37 -0800

    Added multivariate regression.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RegressionUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RegressionUtil.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 
 import com.acmerobotics.roadrunner.kinematics.Kinematics;
 
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 import java.io.File;
@@ -38,6 +39,17 @@ public class RegressionUtil {
 
         public AccelResult(double kA, double rSquare) {
             this.kA = kA;
+            this.rSquare = rSquare;
+        }
+    }
+
+    public static class FeedforwardResult {
+        public final double kV, kA, kStatic, rSquare;
+
+        public FeedforwardResult(double kV, double kA, double kStatic, double rSquare) {
+            this.kV = kV;
+            this.kA = kA;
+            this.kStatic = kStatic;
             this.rSquare = rSquare;
         }
     }
@@ -152,5 +164,38 @@ public class RegressionUtil {
         }
 
         return new AccelResult(Math.abs(accelReg.getSlope()), accelReg.getRSquare());
+    }
+
+    public static FeedforwardResult fitStateData(List<Double> timeSamples, List<Double> velSamples,
+                                                 List<Double> accelSamples, List<Double> powerSamples,
+                                                 @Nullable File file) {
+        if (file != null) {
+            try (PrintWriter pw = new PrintWriter(file)) {
+                pw.println("time,position,power");
+                for (int i = 0; i < timeSamples.size(); i++) {
+                    double time = timeSamples.get(i);
+                    double vel = velSamples.get(i);
+                    double accel = accelSamples.get(i);
+                    double power = powerSamples.get(i);
+                    pw.println(time + ", " + vel + ", " + accel + ", " + power);
+                }
+            } catch (FileNotFoundException e) {
+                // ignore
+            }
+        }
+
+        double[][] x = new double[timeSamples.size()][2];
+        double[] y = new double[powerSamples.size()];
+
+        for (int i = 0; i < timeSamples.size(); i++) {
+            x[i] = new double[]{velSamples.get(i), accelSamples.get(i)};
+            y[i] = powerSamples.get(i);
+        }
+
+        OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+        regression.newSampleData(y, x);
+
+        double[] b = regression.estimateRegressionParameters();
+        return new FeedforwardResult(b[1], b[2], b[0], regression.calculateAdjustedRSquared());
     }
 }
