80eca888694388066a8a24f3fbf4bc0b67c4f677
Jared Haertel <jared.haertel@gmail.com>
2023-01-21 23:42:01 -0800

    Removed generalization.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ReadyPickUp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ReadyPickUp.kt
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.commands
 
 import com.arcrobotics.ftclib.command.InstantCommand
 import com.arcrobotics.ftclib.command.SequentialCommandGroup
+import com.arcrobotics.ftclib.command.WaitUntilCommand
 import org.firstinspires.ftc.teamcode.RobotConfig.passthroughDepositAngle
+import org.firstinspires.ftc.teamcode.RobotConfig.passthroughPickUpAngle
 import org.firstinspires.ftc.teamcode.subsystems.*
 
 /**
@@ -10,13 +12,14 @@ import org.firstinspires.ftc.teamcode.subsystems.*
  * in the right position for pick up. The [Claw] should remain off.
  * @author Jared Haertel
  */
-class ReadyPickUp(lift: Lift, claw: Claw, passthrough: Passthrough, depositAngle: Double = passthroughDepositAngle) : SequentialCommandGroup() {
+class ReadyPickUp(lift: Lift, claw: Claw, passthrough: Passthrough) : SequentialCommandGroup() {
 
     init{
         addCommands(
             InstantCommand(lift::retract, lift),
-            InstantCommand(claw::open, claw),
-            InstantCommand({ passthrough.setpoint = depositAngle }, passthrough)
+            InstantCommand(passthrough::pickUp, passthrough),
+            WaitUntilCommand { passthrough.getAngleEstimate() <= 90.0 }, // TODO it can be > 90
+            InstantCommand(claw::open, claw)
         )
         addRequirements(lift, claw, passthrough)
     }
