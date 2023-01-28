1971a924c24835c89b727333ad23f7c546d86474
Jared Haertel <jared.haertel@gmail.com>
2023-01-27 23:58:19 -0800

    Implemented vision measurement model.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/localization/VisionMeasurementModel.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/localization/VisionMeasurementModel.kt
@@ -1,34 +1,80 @@
 package org.firstinspires.ftc.teamcode.drive.localization
 
 import org.ejml.simple.SimpleMatrix
+import org.firstinspires.ftc.teamcode.RobotConfig
 import org.firstinspires.ftc.teamcode.filters.particleFilter.ProbabilisticMeasurementModel
-import org.firstinspires.ftc.teamcode.subsystems.Vision
-import org.firstinspires.ftc.teamcode.util.matrixToPose
-import org.firstinspires.ftc.teamcode.util.vectorToDoubleArray
+import org.firstinspires.ftc.teamcode.util.matrixToVector
+import org.firstinspires.ftc.teamcode.vision.modules.ContourResults.AnalysisResult
 import org.opencv.core.Point
+import kotlin.math.*
 
-class VisionMeasurementModel(private val vision: Vision) : ProbabilisticMeasurementModel {
-
-    override fun predictObservation(state: SimpleMatrix): SimpleMatrix {
-        TODO("Not yet implemented")
-    }
-
+class VisionMeasurementModel : ProbabilisticMeasurementModel {
     override fun calculateProbability(z: SimpleMatrix, state: SimpleMatrix): Double {
-        val probability = 1.0
+        var probability = 1.0
 
-        // Each column of z is an observation of a pole
-        val expected = predictObservation(state)
-        return 0.0
+        val z = matrixToList(z)
+        val expected = getExpectedObservations(state)
+        val pairs = getCorrespondence(z, expected)
+
+        for (pair in pairs) {
+            probability *= gaussianProbability(pair.first.angle, pair.second.angle, 0.0)
+            probability *= gaussianProbability(pair.first.distance, pair.second.distance, 0.0)
+        }
+
+        // TODO adjust for the different number of observation and when len(expected) != len(z)
+
+        return probability
     }
 
-    fun getCorrespondingLandmarkPosition(z: SimpleMatrix, state: SimpleMatrix) {
-        val landmarks = Array<SimpleMatrix>(z.numCols()) { i ->
-            val pixel = Point(z.get(0, i), z.get(1, i))
-            val realHeight = z.get(2, i)
-            val globalVector = vision.pixelToGlobalPosition(pixel, realHeight, matrixToPose(state))
-            val globalZ = SimpleMatrix(arrayOf(vectorToDoubleArray(globalVector)))
-            globalZ
+    private fun getExpectedObservations(state: SimpleMatrix): List<AnalysisResult> {
+
+        val polesInSight = mutableListOf<AnalysisResult>()
+
+        for (pole in enumValues<RobotConfig.Pole>().toList()) {
+            val diff = pole.vector - matrixToVector(state)
+            val localVec = diff.rotated(-state.get(2))
+
+            val yaw = localVec.angle()
+            val dist = localVec.norm()
+            // TODO Pixel/Projection
+
+            // Only add if pole within camera FOV
+            if (abs(yaw) <= RobotConfig.WEBCAM_FOVX/2.0)
+                polesInSight.add(AnalysisResult(Point(0.0, 0.0), yaw, dist))
         }
+
+        return polesInSight.toList()
+    }
+
+    /**
+     * Assumes tight convergence
+     * TODO GENERALIZE
+     */
+    private fun getCorrespondence(z: List<AnalysisResult>, expected: List<AnalysisResult>): List<Pair<AnalysisResult, AnalysisResult>> {
+        return List(z.size) { i ->
+            val currentObservation = z[i]
+            val closestPole = expected.minBy { distanceSqr(currentObservation, it) }
+            Pair(currentObservation, closestPole)
+        }
+    }
+
+    private fun distanceSqr(p1: AnalysisResult, p2: AnalysisResult): Double {
+        val a = p1.distance
+        val b = p2.distance
+        val dtheta = p1.angle - p2.angle
+        return a*a + b*b - 2*a*b*cos(dtheta)
+    }
+
+    private fun matrixToList(z: SimpleMatrix): List<AnalysisResult> {
+        return List(z.numCols()) {i ->
+            AnalysisResult(Point(z.get(0, i), z.get(1, i)), z.get(2, i), z.get(3, i))
+        }
+    }
+
+    private val denom = 1.0 / sqrt(2*PI)
+    private fun gaussianProbability(observation: Double, expected: Double, standardDeviation: Double): Double {
+        val zScore = (observation - expected) / standardDeviation
+        return exp(-zScore * zScore / 2.0) / standardDeviation * denom
     }
 
 }
