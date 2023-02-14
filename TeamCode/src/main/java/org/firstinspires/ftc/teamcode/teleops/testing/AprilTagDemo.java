90ca497eae13a6f533c5d0c8e6ed3c69c02d77f3
Jared Haertel <jared.haertel@gmail.com>
2023-02-14 09:10:14 -0800

    Moved to testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/testing/AprilTagDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/testing/AprilTagDemo.java
@@ -19,7 +19,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.teleops;
+package org.firstinspires.ftc.teamcode.teleops.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
