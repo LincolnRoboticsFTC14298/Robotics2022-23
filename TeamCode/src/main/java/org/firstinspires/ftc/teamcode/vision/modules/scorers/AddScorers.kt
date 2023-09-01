761eff028d43356833f583f73e6ab18184368e47
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2023-08-31 17:45:43 -0700

    Delete TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/modules directory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/modules/scorers/AddScorers.kt
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.vision.modules.scorers
-
-import org.opencv.core.MatOfPoint
-
-class AddScorers(private vararg val scorers: Scorer) : Scorer {
-    override fun score(contour: MatOfPoint): Double = scorers.sumOf { it.score(contour) }
-}
-
-operator fun Scorer.plus(other: Scorer): Scorer = AddScorers(this, other)
